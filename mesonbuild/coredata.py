1b0666a7a134becd5d294cf35781303fd0b20e31
Andres Rangel <andres.rangel@featuremine.com>
2020-10-20 11:36:05 -0500

    resolution of possible conflicts related to version

--- mesonbuild/coredata.py
+++ mesonbuild/coredata.py
@@ -33,7 +33,7 @@ import enum
 if typing.TYPE_CHECKING:
     from . import dependencies
 
-version = '0.50.9999'
+version = '0.50.999'
 backendlist = ['ninja', 'vs', 'vs2010', 'vs2015', 'vs2017', 'vs2019', 'xcode']
 
 default_yielding = False
